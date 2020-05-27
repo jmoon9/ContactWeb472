4e895d428ac23433c23078ce0d799c6fe0e9de6a
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2020-05-26 20:53:30 -0500

    Added models, migrations, and seed for contacts and states

---
+++ ContactWeb472/Migrations/202005270125202_CreateContactAndStateTables.cs
@@ -0,0 +1,56 @@
+﻿namespace ContactWeb472.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateContactAndStateTables : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contacts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 250),
+                        Email = c.String(nullable: false, maxLength: 250),
+                        PhonePrimary = c.String(nullable: false, maxLength: 15),
+                        PhoneSecondary = c.String(maxLength: 15),
+                        Birthday = c.DateTime(nullable: false),
+                        StreetAddress1 = c.String(maxLength: 250),
+                        StreetAddress2 = c.String(maxLength: 250),
+                        City = c.String(nullable: false, maxLength: 50),
+                        StateId = c.Int(nullable: false),
+                        Zip = c.String(nullable: false, maxLength: 10),
+                        User_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.States", t => t.StateId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.User_Id)
+                .Index(t => t.StateId)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.States",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Abbreviation = c.String(nullable: false, maxLength: 4),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contacts", "User_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Contacts", "StateId", "dbo.States");
+            DropIndex("dbo.Contacts", new[] { "User_Id" });
+            DropIndex("dbo.Contacts", new[] { "StateId" });
+            DropTable("dbo.States");
+            DropTable("dbo.Contacts");
+        }
+    }
+}
---
+++ ContactWeb472/Models/ContactWebConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace ContactWeb472.Models
+{
+    public class ContactWebConstants
+    {
+        public const int MAX_STATE_NAME_LENGTH = 50;
+        public const int MAX_STATE_ABBR_LENGTH = 4;
+
+        public const int MAX_FIRST_NAME_LENGTH = 50;
+        public const int MAX_LAST_NAME_LENGTH = 250;
+        public const int MAX_PHONE_LENGTH = 15;
+        public const int MAX_EMAIL_LENGTH = 250;
+
+        public const int MAX_STREET_ADDR_LENGTH = 250;
+        public const int MAX_CITY_LENGTH = 50;
+
+        public const int MAX_ZIP_LENGTH = 10;
+        public const int MIN_ZIP_LENGTH = 5;
+    }
+}
